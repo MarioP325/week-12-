348f966180366fa9760223967696e2273b7e76dc
MarioP325 <mapantoja1@cps.edu>
2025-11-18 20:04:57 +0000

    sets and tupples

---
+++ sets_tupples.py
@@ -0,0 +1,21 @@
+#Sets and tupples examples 
+#Sets examples: 
+set1 = { 1, 2, 3, 4, 5}
+print(set1)
+print(type(set1))
+set1.add(6)
+print(set1)
+set1.remove(2)
+print(set1)
+
+set2= {"apple", "apple", "banana", "melon"}
+print(set2)
+
+#tupples
+tuple1= (1,2,3,4,5)
+print(tuple1)
+print(type(tuple1))
+#tupples are immutable which means they cannot be changed
+#good for storing unchanging data
+socialsn=(11111,2222,333,4)
+print(socialsn)
