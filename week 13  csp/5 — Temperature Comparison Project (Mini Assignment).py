d70929c8a4af57e291b24d4cf96cf4019504be24
MarioP325 <mapantoja1@cps.edu>
2025-12-01 20:44:53 +0000

    temperature list

--- week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
+++ week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
@@ -11,4 +11,12 @@
 # If the temperature is out of range (below -10 or above 110), display “Extreme temperature warning!”
 
 # Starter Code:
-
+temperature= int(input("What is the temperature outside today"))
+if temperature>10 and temperature<=60:
+    print("the temperature is cold outside")
+elif temperature>=70 and temperature<80:
+    print("The temperature is warm out today")
+elif temperature<=110 and temperature>=90:
+    print("The temperature is hot out today")
+elif temperature<-10 or temperature>110:
+    print("Extreme temperature please beware!!!") 
